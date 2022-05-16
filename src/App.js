72a436e324bd729ec6ca69978e669ca880db2388
Jaydeep Shelake <jaydeepshelake10@gmail.com>
2022-05-16 19:36:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import {BrowserRouter as Router,Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Router>
+            <React.Fragment>
+                <main>
+              <Routes>
+                <Route path='/' element={<Home/>}/>
+              </Routes>
+              </main>
+          </React.Fragment>
+        </Router>
+  )
 }
 
-export default App;
+export default App
