407c44a2e727747f37e9bbd83fac977ea767293e
Jaydeep Shelake <jaydeepshelake10@gmail.com>
2022-05-17 21:40:03 +0530

    some change

---
+++ src/firebase.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore, serverTimestamp } from "firebase/firestore";
+import {getStorage} from 'firebase/storage'
+import { getAuth } from "firebase/auth";
+
+const firebaseApp = initializeApp(
+    {
+        apiKey: "AIzaSyAL59ODaaJkz4DZNA_HbzacePeF_lPUE1o",
+        authDomain: "qpwebsite.firebaseapp.com",
+        projectId: "qpwebsite",
+        storageBucket: "qpwebsite.appspot.com",
+        messagingSenderId: "426471890403",
+        appId: "1:426471890403:web:f76d61ca2c99180dc314d6",
+        measurementId: "G-6WDNGY2SZW"
+    }
+);
+
+ export const firestore = getFirestore(firebaseApp)
+ export const auth = getAuth(firebaseApp)
+export const storage = getStorage(firebaseApp)
+ export const db={
+    
+    formatedDoc:doc=>{
+      return{id:doc.id,...doc.data()}
+    },
+    getCurrentTimeStamp:serverTimestamp,
+  }
